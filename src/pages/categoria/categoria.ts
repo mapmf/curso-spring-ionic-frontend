1a1fe15b854ab9a5d43e9591e22b33ae01478634
Marcos <marcosapmf22@gmail.com>
2020-01-14 21:31:43 -0300

    showing categories

--- src/pages/categoria/categoria.ts
+++ src/pages/categoria/categoria.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../app/services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 
 @IonicPage()
@@ -9,6 +11,9 @@ import { CategoriaService } from '../../app/services/domain/categoria.service';
   templateUrl: 'categoria.html',
 })
 export class CategoriaPage {
+
+  items : CategoriaDTO[];
+  bucketUrl : string = API_CONFIG.bucketBaseUrl;
 
   constructor(
     public navCtrl: NavController, 
@@ -19,7 +24,7 @@ export class CategoriaPage {
   ionViewDidLoad() {
 
     this.categoriaService.findAll().subscribe(response => {
-      console.log(response);
+      this.items = response;
     },
     error => {
       console.log(error);
